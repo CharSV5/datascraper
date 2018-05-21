a20489f03b0e683aef0b98fd5b23c9ac85641704
CharSV5 <charlenebastians@hotmail.com>
2018-05-21 11:01:13 +0100

    basic venue method currently failing, needs refactoring

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,4 +13,9 @@ class Scraper
   def get_city(concert)
     concert.css('.venue-details').text.split(':')[0]
   end
+
+  def get_venue(get_location)
+    location = concert.css('.venue-details').text.split(':')[1]
+    location.split("\n")[0] unless location.nil?
+  end
 end
